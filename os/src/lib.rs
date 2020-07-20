1e8abe23971f5946accffb482e6725da64d3f8a2
二手掉包工程师 <rustin.liu@gmail.com>
2020-07-20 23:58:38 +0800

    feat(4): finish chapter 4 (#3)

--- os/src/lib.rs
+++ os/src/lib.rs
@@ -1,12 +1,17 @@
 #![no_std]
 #![feature(llvm_asm)]
 #![feature(global_asm)]
+#![feature(alloc_error_handler)]
+
+extern crate alloc;
 
 #[macro_use]
 mod io;
+mod consts;
 mod context;
 mod init;
 mod interrupt;
 mod lang_items;
+mod memory;
 mod sbi;
 mod timer;
